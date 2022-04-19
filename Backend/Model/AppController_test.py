b623ea9ee0a885041606ca821a79d1a3dd0266e7
PIN-ACH <pinakkokcha1@gmail.com>
2022-04-19 09:33:43 +0100

    fundadd and delete

--- Backend/Model/AppController_test.py
+++ Backend/Model/AppController_test.py
@@ -10,15 +10,15 @@ class APPTest(unittest.TestCase):
     find_fund_URL = "{}/funds/HDFC".format(testURL)
     risk_URL = "{}/fundsRisk".format(testURL)
     fundbyid_URL = "{}/fundsById/1".format(testURL)
-    #/userfund/add
-    #http://52.176.47.148:8080/funddelete
+    userfundadd_URL= "{}/userfund/add".format(testURL)
+    funddelete_URL="{}/funddelete".format(testURL)
     headers = {'Content-Type': 'application/json', 'authorization': 'Basic dXNlcjphZG1pbg=='}
 
     jsonsignup={
   "userId": 0,
-  "userName": "test9",                              #imp change user name
-  "userEmail": "test9@xya.com",                     #imp change user email
-  "userPassword": "test9",                          #imp change pass
+  "userName": "test13",                              #imp change user name
+  "userEmail": "test13@xya.com",                     #imp change user email
+  "userPassword": "test13",                          #imp change pass
   "userPhoneNumber": "108497858",
   "userAge": "81",
   "imgSrc": ""
@@ -45,7 +45,7 @@ class APPTest(unittest.TestCase):
         "fundDesc": "Equity",
         "imgSrc": "https://imageio.forbes.com/i-forbesimg/media/lists/companies/hdfc-bank_416x416.jpg?format=jpg&height=416&width=416&fit=bounds"
     }
-]
+    ]
     risk_list = [
     "low",
     "high",
@@ -56,15 +56,15 @@ class APPTest(unittest.TestCase):
     "ufId": 0,
     "fundId": 2,
     "ufType": "Equity",
-    "ufDate": "Mon Apr 18 2022",
-    "ufAmount": 45,
+    "ufDate": "Mon Apr 19 2022",
+    "ufAmount": 10,
     "fundName": "",
     "fundAmc": "",
     "totalFund": 0
 }
     responseuserfundadd={"fundAmc":"","fundId":2,"fundName":"","totalFund":0,"ufAmount":45,"ufDate":"Mon Apr 18 2022","ufId":0,"ufType":"Equity","userId":14}
 
-    deletefund= {"ufId": "4", "userid": "14"}
+    deletefund= {"ufId": "12", "userid": "14"}
 
     def test_1_fundsall(self):  #fetch and check quantity of json document for all funds and check for status
         r = requests.get(APPTest.all_fund_URL)
@@ -102,15 +102,15 @@ class APPTest(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
         self.assertEqual(len(r.json()), 1)
 
-    # def test_9_userfundadd(self):                  #add header
-    #     r = requests.get(APPTest.fundbyid_URL)
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertEqual(len(r.json()), 1)
+    def test_9_userfundadd(self):                  #add user fund
+        r = requests.post(APPTest.userfundadd_URL, headers=self.headers,json=APPTest.useradddata)
+        self.assertEqual(r.status_code, 200)
+        
 
-    # def test_10_userfunddelete(self):                  #add header
-    #     r = requests.get(APPTest.fundbyid_URL)
-    #     self.assertEqual(r.status_code, 200)
-    #     self.assertEqual(len(r.json()), 1)
+    def test_10_userfunddelete(self):                  #delete user fund
+        r = requests.delete(APPTest.funddelete_URL,headers=self.headers,json=APPTest.deletefund)
+        self.assertEqual(r.status_code, 200)
+        self.assertDictEqual(r.json(), APPTest.deletefund)
 
 
 
